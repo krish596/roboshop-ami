2d9a378374da83d5643522249b8079965d4f26e9
Chimtamreddy <kr7348202@gmail.com>
2024-09-14 12:43:33 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "ami" {
-  ami  = [data.aws_ami.ami.id]
+  ami  = data.aws_ami.ami.id
   instance_type = "t3.small"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
 }
